1373e65ad41dfba7b1cd512361140a178ca39560
nahid <mnh_milu@yahoo.com>
2016-02-09 23:18:58 +0600

    first commit

---
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan({"demo","controller"})
+
+public class DemoApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
---
+++ demo/src/main/java/controller/PersonController.java
@@ -0,0 +1,37 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import model.Person;
+
+
+@Controller
+public class PersonController {
+	
+	@RequestMapping("/person")
+	public String person(Model model){
+		
+		Person p = new Person();
+		p.setFirstName("Nahid");
+		p.setLastName("Hossain");
+		
+		model.addAttribute("person",p);
+		return "personview";	
+		
+		
+		
+	}
+	@ResponseBody
+	@RequestMapping("/")
+	String entry(){		
+		return "My Spring Bood App";		
+	}
+	
+	
+	
+	
+
+}
