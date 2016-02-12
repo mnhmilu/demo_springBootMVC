a23d5876fe0d61d98ec178e4b14f8902b73c4630
nahid <nahid@nahid-laptop>
2016-02-12 23:39:44 +0600

    kkk

--- demo/src/main/java/com/test/SpringBootWebApplication.java
+++ demo/src/main/java/com/test/SpringBootWebApplication.java
@@ -1,7 +1,5 @@
 package com.test;
 
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,24 +8,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan({"demo","controller","configuration","bootstrap","repositories","domain"})
-//@ComponentScan({"configuration"})
+
 public class SpringBootWebApplication {
 
-    public static void main(String[] args) {
-    	
-	ApplicationContext ctx =SpringApplication.run(SpringBootWebApplication.class, args);
-		
-		//for(String s : ctx.getBeanDefinitionNames())
-		//{
-		//	System.out.println(s);
-		//}
- 
-		System.out.println("Bean count:" +ctx.getBeanDefinitionCount());
-		//https://springframework.guru/spring-boot-web-application-part-3-spring-data-jpa/
-		
-		
-    }
+	public static void main(String[] args) {
+
+		// https://springframework.guru/spring-boot-web-application-part-3-spring-data-jpa/
+
+		ApplicationContext ctx = SpringApplication.run(SpringBootWebApplication.class, args);
+
+	}
 }
