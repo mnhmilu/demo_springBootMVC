5e378f473ac084d6069dec404679def7792f2add
nahid <mnh_milu@yahoo.com>
2016-05-13 21:31:09 +0600

    Complete example of mvc

---
+++ demo/src/main/java/com/test/services/ProductService.java
@@ -0,0 +1,14 @@
+package com.test.services;
+
+
+import com.test.domain.Product;
+
+public interface ProductService {
+    Iterable<Product> listAllProducts();
+
+    Product getProductById(Integer id);
+
+    Product saveProduct(Product product);
+
+    void deleteProduct(Integer id);
+}
