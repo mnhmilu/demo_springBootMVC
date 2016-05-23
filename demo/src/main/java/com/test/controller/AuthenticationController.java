06818fd8e64e981ccfef464458030f9fad9e16fd
nahid <mnh_milu@yahoo.com>
2016-05-24 02:23:47 +0600

    Basic Security Implemented

--- demo/src/main/java/com/test/controller/AuthenticationController.java
+++ demo/src/main/java/com/test/controller/AuthenticationController.java
@@ -3,18 +3,23 @@ package com.test.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.test.domain.PatientProfile;
 
 
 @Controller
 public class AuthenticationController {
+//	
+//	@RequestMapping("/")
+//	public String index(Model model){		
+//		
+//		return "index";	
+//		
+//	}	
 	
-	@RequestMapping("/")
-	public String person(Model model){		
-		
-		return "index";	
-		
-	}
+	
 	@ResponseBody
 	@RequestMapping("/test")
 	String entry(){		
