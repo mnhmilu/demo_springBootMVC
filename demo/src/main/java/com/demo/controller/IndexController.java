b98526cf4fb0ed0c062f8d85bb290d84c6a70ff7
nahid <mnh_milu@yahoo.com>
2016-11-28 07:43:44 +0600

    General Search added

--- demo/src/main/java/com/demo/controller/IndexController.java
+++ demo/src/main/java/com/demo/controller/IndexController.java
@@ -1,31 +1,74 @@
 package com.demo.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.List;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.demo.commands.DrugForm;
+import com.demo.commands.DrugSearchForm;
+import com.demo.converter.DrugDataToDrugForm;
+import com.demo.domain.Drug;
+import com.demo.repositories.DrugRepository;
+
+import scala.annotation.meta.getter;
+
 @Controller
 public class IndexController {
+
+	private DrugRepository drugDaoService;
+	private DrugDataToDrugForm drugDataToDrugForm;
 	
-	
-	
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Model model) {
-		
-		return "index";
-		
-	}
+	private final Logger slf4jLogger = LoggerFactory.getLogger(DrugController.class);
 	
 
+	@Autowired
+	public void setservices(DrugRepository drugDaoService,DrugDataToDrugForm drugDataToDrugForm) {
+
+		this.drugDaoService = drugDaoService;
+		this.drugDataToDrugForm = drugDataToDrugForm;
+
+	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+
+		model.addAttribute("drug", new DrugSearchForm());
+		return "index";
+
+	}
+
+	@RequestMapping(value = "/drugSearchFromIndex", method = RequestMethod.POST)
+	public String drugSearch(DrugSearchForm form, BindingResult bindingResult, Model model) {
+
+		if (form.getDrugName() == null || form.getDrugName().trim()=="") {
+			return "redirect:/";
+
+		}
+
+		List<Drug> drugsSearchResult = drugDaoService.findDrugByDrugManufacturerOrByDrugGenericOrDrugName(null, "",
+				form.getDrugName());
+
+		model.addAttribute("drugs", drugsSearchResult);
+		return "drugs/drugsGenericSearch";
+
+	}
 	
-	
+	@RequestMapping("/index/drugDeatails/{id}")
+	public String showDrug(@PathVariable Integer id, Model model) {
+
+		slf4jLogger.info("DrugController :: showDrug");
+
+		DrugForm form = drugDataToDrugForm.convert(drugDaoService.findById(id));
+		model.addAttribute("drug", form);
+		return "drugs/drugDetailsGeneral";
+	}
 
 }
