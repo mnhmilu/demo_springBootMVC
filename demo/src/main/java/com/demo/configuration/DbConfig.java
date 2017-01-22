1727fd5a44db4c66b8c18a722d33ebee76805013
nahid <mnh_milu@yahoo.com>
2017-01-22 22:28:33 +0600

    jjjj

--- demo/src/main/java/com/demo/configuration/DbConfig.java
+++ demo/src/main/java/com/demo/configuration/DbConfig.java
@@ -8,12 +8,15 @@ import java.sql.SQLException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class DbConfig {
 
-	@Bean
-	public Connection getConnection() throws URISyntaxException, SQLException {
+	//@Bean
+	public Connection getConnection() throws URISyntaxException, SQLException, ClassNotFoundException {
 		String urlDB = System.getenv("JDBC_DATABASE_URL");
+		Class.forName("org.postgresql.Driver");	
+		urlDB="jdbc:postgres://ec2-54-235-240-92.compute-1.amazonaws.com:5432/d3qgc1dvqq0ilh";
+		System.out.println("nahid:"+ urlDB);
 		return DriverManager.getConnection(urlDB);
 	}
 
